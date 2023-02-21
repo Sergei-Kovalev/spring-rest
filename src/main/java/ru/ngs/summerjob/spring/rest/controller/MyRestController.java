87c113604ffeab91394fede17461715e347396ec
Sergei Kovalev <summerjob@ngs.ru>
2023-02-21 16:52:37 +0300

    Update & Delete added.

--- src/main/java/ru/ngs/summerjob/spring/rest/controller/MyRestController.java
+++ src/main/java/ru/ngs/summerjob/spring/rest/controller/MyRestController.java
@@ -40,6 +40,26 @@ public class MyRestController {
         return employee;
     }
 
+    @PutMapping("/employees")
+    public Employee changeEmployee(@RequestBody Employee employee) {
+
+        employeeService.saveEmployee(employee);
+
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployeeById(id);
+        if (employee == null) {
+            throw new NoSuchEmployeeException("Employee with id " + id + " does not exist!");
+        }
+
+        employeeService.deleteEmployee(id);
+
+        return "Employee with id " + id + " was deleted!";
+    }
+
     //just test method...
     @RequestMapping("/")
     public String showStart() {
